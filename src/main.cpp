0a6500a9ecb9b438fdf362ccef0db662d1b78d89
Vadims Zinatulins <vadimz2@hotmail.com>
2022-02-28 18:21:03 +0000

    Version 0.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
+#include "config.h"
+
 #include <iostream>
+#include <string>
+#include <filesystem>
+
+void generateWorkspace(const std::string &projectName)
+{
+	std::filesystem::create_directory(projectName);
+	std::filesystem::current_path(std::filesystem::current_path().string() + "/" + projectName);
+	for(const auto &dir : { "bin", "build", "config", "include", "src" }) std::filesystem::create_directory(dir);
+}
 
 int main(int argc, char *argv[])
 {
-	std::cout << "Hello World!" << std::endl;
+	std::cout << "Running gbgen version: " << MAJOR_VERSION << "." << MINOR_VERSION << std::endl;
+	if(argc != 2)
+	{
+		std::cout << "Wrong argument count" << std::endl;
+		std::cout << "Usage: gbgen [PROJECT_NAME]" << std::endl;
+
+		return 1;
+	}
+
+	std::string projectName(argv[1]);
+
+	generateWorkspace(projectName);
+
 	return 0;
 }
