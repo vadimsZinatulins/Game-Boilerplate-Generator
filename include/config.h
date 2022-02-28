2545e4be8dfb40a0994a601dbcf7df43d804f2e3
Vadims Zinatulins <vadimz2@hotmail.com>
2022-02-28 23:02:00 +0000

    Version 0.8

--- include/config.h
+++ include/config.h
@@ -1,4 +1,4 @@
 #pragma once
 
 #define MAJOR_VERSION 0
-#define MINOR_VERSION 6
+#define MINOR_VERSION 7
--- src/main.cpp
+++ src/main.cpp
@@ -22,13 +22,27 @@ int main(int argc, char *argv[])
 
 	std::string projectName(argv[1]);
 
+	std::cout << "[1/7] Generating workspace" << std::endl;
 	generateWorkspace(projectName);
+	std::cout << "[2/7] Generating CMakeLists.txt" << std::endl;
 	generateCMakeLists(projectName);
+	std::cout << "[3/7] Generating KeyManager class" << std::endl;
 	generateKeyManager();
+	std::cout << "[4/7] Generating Game class" << std::endl;
 	generateGameClass();
+	std::cout << "[5/7] Generating " << projectName << " class" << std::endl;
 	generateProjectNameClass(projectName);
+	std::cout << "[6/7] Generating src/main.cpp" << std::endl;
 	generateMain(projectName);
+	std::cout << "[7/7] Generating config/config.h.in" << std::endl;
 	generateConfigFile(projectName);
+
+	std::cout << "Done! You can now run the following commands:" << std::endl;
+	std::cout << "\tcd " << projectName << std::endl;
+	std::cout << "\tcmake -E chdir build/ cmake .." << std::endl;
+	std::cout << "\tcmake --build build/" << std::endl << std::endl;
+	std::cout << "\tbin/" << projectName << std::endl << std::endl;
+	std::cout << "And change the both config/config.h.in and CMakeLists.txt files as you want. Have fun!" << std::endl;
 
 	return 0;
 }
