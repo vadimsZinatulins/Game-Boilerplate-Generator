f1861a21efe871d3d3b7b54f9473328da3a43558
Vadims Zinatulins <vadimz2@hotmail.com>
2022-03-04 23:50:10 +0000

    Fixed a bug where nextFloat methods from Random class where returning ints

--- src/randomclass.cpp
+++ src/randomclass.cpp
@@ -16,9 +16,9 @@ void generateRandomHeader()
 	header << "\tint nextInt();\n";
 	header << "\tint nextInt(int max);\n";
 	header << "\tint nextInt(int min, int max);\n\n";
-	header << "\tint nextFloat();\n";
-	header << "\tint nextFloat(float max);\n";
-	header << "\tint nextFloat(float min, float max);\n";
+	header << "\tfloat nextFloat();\n";
+	header << "\tfloat nextFloat(float max);\n";
+	header << "\tfloat nextFloat(float min, float max);\n";
 	header << "private:\n";
 	header << "\tstd::mt19937 m_engine;\n";
 	header << "};\n";
@@ -39,9 +39,9 @@ void generateRandomSource()
 	src << "int Random::nextInt() { return nextInt(0, std::numeric_limits<int>::max()); }\n";
 	src << "int Random::nextInt(int max) { return nextInt(0, max); }\n";
 	src << "int Random::nextInt(int min, int max) { return std::uniform_int_distribution<int>(min, max)(m_engine); }\n\n";
-	src << "int Random::nextFloat() { return nextFloat(0, std::numeric_limits<float>::max()); }\n";
-	src << "int Random::nextFloat(float max) { return nextFloat(0, max); }\n";
-	src << "int Random::nextFloat(float min, float max) { return std::uniform_real_distribution<float>(min, max)(m_engine); }\n";
+	src << "float Random::nextFloat() { return nextFloat(0, std::numeric_limits<float>::max()); }\n";
+	src << "float Random::nextFloat(float max) { return nextFloat(0, max); }\n";
+	src << "float Random::nextFloat(float min, float max) { return std::uniform_real_distribution<float>(min, max)(m_engine); }\n";
 
 	src.close();
 }
