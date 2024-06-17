b77aaf067959f5e3dd2fb3963dc447fba4a1f1b8
Vadims Zinatulins <vadimz2@hotmail.com>
2024-06-17 19:06:08 +0100

    Extending vulkan functionalities

--- include/templates/vulkan/VulkanInstanceTemplate.h
+++ include/templates/vulkan/VulkanInstanceTemplate.h
@@ -49,8 +49,96 @@ const auto VULKAN_INSTANCE_CPP_TEMPLATE { R"(#include "be/vulkan/Instance.h"
 #include <vulkan/vk_enum_string_helper.h>
 #include <string>
 #include <iostream>
+#include <algorithm>
+#include <numeric>
 
 namespace be::vulkan {
+
+namespace local_helpers {
+    
+void checkExtensionsSupport(const std::vector<const char*> &checkExtensions) {
+    // Get the extensions properties count
+    uint32_t extensionCount { 0 };
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    // Get the extensions properties
+    std::vector<VkExtensionProperties> extensions(extensionCount);
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+    // Get the list of all unsupported extensions
+    std::vector<std::string> unsupportedExtensions;
+    for(const auto &checkExtension : checkExtensions) {
+        bool hasExtension { std::any_of(extensions.begin(), extensions.end(), [checkExtension](const auto &extension) {
+            return strcmp(checkExtension, extension.extensionName) == 0;
+        }) };
+
+        if(!hasExtension) {
+            unsupportedExtensions.push_back(checkExtension);
+        }
+    }
+
+    if(!unsupportedExtensions.empty()) {
+        // Lambda that accumulates all unsupported extensions (comma-separated)
+        const auto accumulator { [](const std::string &a, const std::string &b){
+            return a + ", " + b;
+        } };
+
+        // String message that lists all unsupported extensions
+        std::string errorMessage {
+            "Unsupported Extensions: " + std::accumulate(
+                unsupportedExtensions.begin() + 1, 
+                unsupportedExtensions.end(), 
+                unsupportedExtensions[0], 
+                accumulator
+            )
+        };
+
+        throw std::runtime_error(errorMessage);
+    }
+}
+
+void checkValidationLayersSupport(const std::vector<const char*> &layers) {
+    // Get the layers count
+    uint32_t layerCount { 0 };
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    // Get the layers
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    // Get the list of all unsupported layers
+    std::vector<std::string> unsupportedLayers;
+    for(const auto &checkLayer : layers) {
+        bool hasExtension { std::any_of(availableLayers.begin(), availableLayers.end(), [checkLayer](const auto &layer) {
+            return strcmp(checkLayer, layer.layerName) == 0;
+        }) };
+
+        if(!hasExtension) {
+            unsupportedLayers.push_back(checkLayer);
+        }
+    }
+
+    if(!unsupportedLayers.empty()) {
+        // Lambda that accumulates all unsupported layers (comma-separated)
+        const auto accumulator { [](const std::string &a, const std::string &b){
+            return a + ", " + b;
+        } };
+
+        // String message that lists all unsupported layers
+        std::string errorMessage {
+            "Unsupported Validation Layers: " + std::accumulate(
+                unsupportedLayers.begin() + 1, 
+                unsupportedLayers.end(), 
+                unsupportedLayers[0], 
+                accumulator
+            )
+        };
+
+        throw std::runtime_error(errorMessage);
+    }
+}
+
+}
 
 Instance::operator VkInstance() const {
     return m_instance;
@@ -61,7 +149,7 @@ Instance::Instance(SDL_Window *window, bool enableDebug) {
     VkApplicationInfo appInfo {};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     // Application information
-    appInfo.pApplicationName = "{PROJECT_NAME}";
+    appInfo.pApplicationName = "My Vulkan Project";
     appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
     // Engine information
     appInfo.pEngineName = "Basic Engine";
@@ -93,6 +181,11 @@ Instance::Instance(SDL_Window *window, bool enableDebug) {
         // Populate Debug creation information if debug is enabled
         Debug::populateDebugCreateInfo(debugCreateInfo);
     }
+    
+    // Make sure all extensions are supported
+    local_helpers::checkExtensionsSupport(instanceExtensions);
+    // Make sure all validation layers are supported
+    local_helpers::checkValidationLayersSupport(validationLayers);
 
     // Instance creation information
     VkInstanceCreateInfo createInfo {};
@@ -120,4 +213,5 @@ Instance::~Instance() {
     }
 }
 
-})" };
+}
+)" };
